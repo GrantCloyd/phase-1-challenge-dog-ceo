99e3452dddf96c341b5e88ab932667b0059e1ac7
Grant Cloyd <dgcloyd@gmail.com>
2021-05-25 07:58:35 -0500

    commit tree 671d3ac46609a1ca03cc7a87b2580a5ab188434c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-console.log('%c HI', 'color: firebrick')
+const breedDropDown = document.querySelector("#breed-dropdown")
+breedDropDown.addEventListener("change", filterBreeds)
+
+function getData(stringURL) {
+   return fetch(`https://dog.ceo/api/breeds/${stringURL}`)
+      .then(resp => resp.json())
+      .then(data => data)
+}
+
+function createDogStuff(dog, type) {
+   if (type === "img") {
+      const imageLoc = document.querySelector("#dog-image-container")
+      let dogImage = document.createElement("img")
+      dogImage.src = dog
+      imageLoc.append(dogImage)
+   }
+   if (type === "li") {
+      const ulLoc = document.querySelector("#dog-breeds")
+      let li = document.createElement("li")
+      li.innerHTML = dog
+      li.addEventListener("click", () => (li.style.color = "teal"))
+      ulLoc.append(li)
+   }
+}
+
+// a filter with a string variable through interpolation
+// that takes the value of the button -- that would probably mean you don't need a switch case at all
+
+function filterBreeds(e) {
+   let value = e.target.value
+   const dogSearch = document.querySelector("#dog-breeds")
+   dogSearch.innerHTML = ""
+   getData("list/all").then(data => {
+      let dogNameObj = data.message
+      dogNameArr = Object.keys(dogNameObj)
+      dogNameArr.filter(dog => dog.startsWith(`${value}`)).map(dog => createDogStuff(dog, "li"))
+   })
+}
+
+function init() {
+   getData("image/random/4").then(data => {
+      let dogArr = Array.from(data.message)
+      dogArr.map(dog => createDogStuff(dog, "img"))
+   })
+   getData("list/all").then(data => {
+      let dogNameObj = data.message
+      for (dog in dogNameObj) {
+         createDogStuff(dog, "li")
+      }
+   })
+}
+
+init()
